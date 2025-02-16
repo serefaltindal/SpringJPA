d15632a9bd9a559b1be70842ee7ff3ff5373af46
seref <seref@DESKTOP-KFOKDA0>
2025-02-16 14:21:13 +0300

    jpa-query

--- src/main/java/com/altindal/SpringJPA/repository/StudentRepository.java
+++ src/main/java/com/altindal/SpringJPA/repository/StudentRepository.java
@@ -1,9 +1,16 @@
 package com.altindal.SpringJPA.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.altindal.SpringJPA.entitites.Student;
 
 public interface StudentRepository extends JpaRepository<Student, Integer> {
+	
+	/*Query anatasyonunun çalışma mantığı*/
+	@Query(value="from Student", nativeQuery = false)
+	List<Student> findAllStudents();
 
 }
